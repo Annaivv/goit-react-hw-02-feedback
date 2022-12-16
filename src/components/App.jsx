a444b50785fe8f2df71b7ee5a12cdedcb64492d9
Annaivv <annashalya@gmail.com>
2022-12-16 23:32:58 +0200

    feedback basic logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import '../index.css';
+import Feedback from './Feedback/Feedback';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Please leave feedback</h1>
+      <Feedback />
+    </>
   );
 };
